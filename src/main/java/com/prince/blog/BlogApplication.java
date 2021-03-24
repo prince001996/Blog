6d4d014ab454164a2e708b71555d21003d774a37
Prince Kumar <pkprince48@gmaoil.com>
2021-03-24 18:23:32 +0530

    Comments feature added and mapped to user and blogs

--- src/main/java/com/prince/blog/BlogApplication.java
+++ src/main/java/com/prince/blog/BlogApplication.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
 @SpringBootApplication
+//@EnableWebSecurity
 public class BlogApplication {
 
 //    @Autowired
